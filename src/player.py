4b9f44c43ac4de2980d7da983549f35f66b8351a
trucane <trufashionista2013@gmail.com>
2019-10-02 14:19:03 -0500

    inital commit created room and player classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, current_room,):
+        self.name = name
+        self.inventory = inventory
+        self.current_room = current_room
+
+    def _str_(self):
+        return f"My name is {self.name} and I am in room {self.current_room}"
+
+    def __repr__(self):
+        return f"Player({repr(self.name)}, {repr(self.current_room)})"
+
+
+        
--- src/room.py
+++ src/room.py
@@ -1,2 +1,14 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def _init_(self, name, description ):
+        self.name = name
+        self.description = description
+
+    def _str_(self):
+        return f"You are in room {self.name} the {self.description}"
+
+    def _repr(self):
+        return f"Room({repr(self.name)}, {repr(self.description)})"
